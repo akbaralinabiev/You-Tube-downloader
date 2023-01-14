bcf7d598ff38a37c772dce21999a3889954dde47
akbaralinabiev <akbaralinabiev14@gmail.com>
2023-01-14 02:51:31 +0100

    first commit

---
+++ source.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+import os
+
+# URL of the video to download
+url = "https://youtu.be/gYVg80ptU0M"
+
+# Download the video
+yt = YouTube(url)
+video = yt.streams.filter(res='1080p').first()
+video.download(os.getcwd())
+
+print("Video downloaded successfully!")
